86e40c13e638ee17c180460cdc2e9c7de6668874
tom <tom.ignarri@gmail.com>
2019-12-11 18:50:14 +0000

    edit campground route can find campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -59,21 +59,24 @@ router.get("/:id", function(req, res){
 
 //EDIT CAMPGROUND ROUTE
 router.get("/:id/edit", middleware.checkCampgroundOwnership, function(req, res){
-	Campground.findById(req.params.id, function(err, foundCampground){ 
-				
-		res.render("campgrounds/edit", {campground: foundCampground});
-	});
+	Campground.findById(req.params.id, function(err, foundCampground){
+           if (err){
+               req.flash("error", "Campground not found!");
+           } else {
+          res.render("campgrounds/edit", {campground: foundCampground});
+           }
+    });
 });
 
 //UPDATE CAMPGROUND ROUTE - find and update the correct campground
 router.put("/:id", middleware.checkCampgroundOwnership, function(req, res){
 	Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
-		if(err){
-		   res.redirect("/campgrounds");
-		} else {
-			res.render("campgrounds/" + req.params.id);
-		}
-	});	   
+      if(err){
+          res.redirect("/campgrounds");
+      } else {
+          res.redirect("/campgrounds/" + req.params.id);
+      }
+   });
 });
 
 
